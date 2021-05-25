fa8833739ef97e4923d1cafdeb6c979fe167195f
Amadeu Lee <76532707+amadeulee@users.noreply.github.com>
2021-05-24 22:10:32 -0300

    Step 3

--- Projeto-em-JS-React/src/components/Account/index.tsx
+++ Projeto-em-JS-React/src/components/Account/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Modal from 'react-modal';
 import logo1Img from '../../assets/logo1.svg'
 
-import { LoginForm, LoginContainer} from './styles'
+import { LoginForm, LoginContainer, SignUp} from './styles'
 
 Modal.setAppElement('#root');
 interface Transaction{
@@ -26,7 +26,7 @@ export function  Account() {
     const [ createBool, setCreateBool ] = useState(false);
     const [ isPasswordWrong, setIsPasswordWrong ] = useState(false);
     const [ isCpfWrong, setIsCpfWrong ] = useState(false);
-    const [ step, setStep ] = useState(1);
+    const [ step, setStep ] = useState(3);
     const [ cpf, setCpf] = useState('');
 
     if(step===1){
@@ -53,5 +53,31 @@ export function  Account() {
             </>
         )
     }
+
+    if(step===3){
+        return(
+            <>
+                <LoginContainer>
+                    <SignUp isWrong={isPasswordWrong} >
+                        <h2>Criar nova conta</h2>
+                        <input placeholder="Digite seu Nome Completo" type="text" required name="name" onChange={()=> {setIsPasswordWrong(false); setIsCpfWrong(false)}}/>
+                        <input placeholder="Digite seu CPF" type="text" maxLength={11} minLength={11} required name="cpf" onChange={()=> {setIsPasswordWrong(false); setIsCpfWrong(false)}}/>
+                        <input placeholder="Digite a sua senha" type="password" name="password" required onChange={()=> {setIsPasswordWrong(false); setIsCpfWrong(false)}}/>
+                        <input placeholder="Confirme a sua senha" type="password" name="password2" required onChange={()=> {setIsPasswordWrong(false); setIsCpfWrong(false)}}/>
+                        <button type="submit">Enviar para análise</button>
+                        <button type="button" >Voltar</button>
+                    </SignUp>
+                </LoginContainer>
+                <Modal 
+                    isOpen={isTransactionModalOpen}
+                    overlayClassName="react-modal-overlay"
+                    className="react-modal-content"
+                >
+                    <h2>Conta enviada para análise</h2>
+                </Modal>
+            </>
+        )
+    }
+
     return null;
 }
--- Projeto-em-JS-React/src/components/Account/styles.ts
+++ Projeto-em-JS-React/src/components/Account/styles.ts
@@ -86,3 +86,83 @@ export const LoginForm = styled.form<LoginContainerProps>`
 
     }
 `
+
+interface SignUpProps{
+    isWrong: boolean;
+}
+
+export const SignUp = styled.form<SignUpProps>`
+    display: flex;
+    flex-direction: column;
+    width:100%;
+    max-width: 600px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+
+    h2{
+        color: var(--text-title);
+        font-size: 1.5rem;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+
+    h3{
+        color: red;
+        font-size: 1rem;
+    }
+
+    input{
+        width:100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+        border-radius: 0.25rem;
+        border: 1px solid #d7d7d7;
+        background: #e7e9ee;
+
+        font-weight:400;
+        font-size:1rem;
+
+        &::placeholder{
+            color: var(--text-body)
+        }
+
+        & + input{
+            margin-top: 1rem;
+        }
+
+        &.senha{
+            border: 1px solid ${(props)=>props.isWrong? 'red' : '#d7d7d7'};
+
+            &::placeholder{
+                color: ${(props)=>props.isWrong? 'red' : 'var(--text-body)'}
+            }
+        }
+
+    }
+
+    button{
+        width: 100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+        background: var(--green);
+        color: #fff;
+        border-radius: 0.25rem;
+        border:0;
+        font-size:1rem;
+        font-weight:600;
+        margin-top: 1.5rem;
+
+        transition: filter 0.2s;
+
+        & + button{
+            margin-top: 0.5rem;
+        }
+
+        &:hover{
+            filter: brightness(0.9);
+        }
+
+    }
+`
