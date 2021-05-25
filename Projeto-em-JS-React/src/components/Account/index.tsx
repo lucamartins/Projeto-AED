287f7c7b2e3c88eda0576206a31f72aa11ce27b8
Lee <josecarlos_lee@hotmail.com>
2021-05-24 21:51:05 -0300

    step 1

--- Projeto-em-JS-React/src/components/Account/index.tsx
+++ Projeto-em-JS-React/src/components/Account/index.tsx
@@ -1,7 +1,57 @@
-import * as S from './styles'
+import { useState } from 'react'
+import Modal from 'react-modal';
+import logo1Img from '../../assets/logo1.svg'
+
+import { LoginForm, LoginContainer} from './styles'
+
+Modal.setAppElement('#root');
+interface Transaction{
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+    createdAt: Date,
+}
+
+interface AccountProps{
+    cpf: string,
+    name: string,
+    password : string,
+    transactions : Transaction[]
+}
 
 export function  Account() { 
-    return(
-        <h1>Olá doidão</h1>
-    )
+    const [ isTransactionModalOpen, setIsTransactionModalOpen ] = useState(false);
+    const [ password, setPassword ] = useState('');
+    const [ createBool, setCreateBool ] = useState(false);
+    const [ isPasswordWrong, setIsPasswordWrong ] = useState(false);
+    const [ isCpfWrong, setIsCpfWrong ] = useState(false);
+    const [ step, setStep ] = useState(1);
+    const [ cpf, setCpf] = useState('');
+
+    if(step===1){
+        return(
+            <>
+                <LoginContainer>
+                    <LoginForm isWrong={isPasswordWrong} isCpfWrong={isCpfWrong}>
+                        <img src={logo1Img} alt="AJL Bank"/>
+                        <h2>Olá, Seja bem vindo</h2>
+                        <input placeholder={isCpfWrong? "CPF não encontrado" : "Digite seu CPF"} type="number" maxLength={11}  value={cpf} onChange={(e) => {if(e.target.value.length<12)setCpf(e.target.value); setIsCpfWrong(false) }} />
+                        <input placeholder={isPasswordWrong ? "Senha incorreta":"Digite a sua senha" } className="senha" value={password} type="password" onChange={(e) => setPassword(e.target.value)}/>
+                        <button type="button" onClick={()=> console.log('doido')}>Login</button>
+                        <button type="button" onClick={()=> {setStep(3); setIsCpfWrong(false)}}>Criar nova conta</button>
+                    </LoginForm>
+                </LoginContainer>
+                <Modal 
+                    isOpen={isTransactionModalOpen}
+                    overlayClassName="react-modal-overlay"
+                    className="react-modal-content"
+                >   
+                    {createBool && (<h2>Conta criada com sucesso</h2>)}
+                    {!createBool && (<h2>Conta encerrada com sucesso</h2>)}
+                </Modal>
+            </>
+        )
+    }
+    return null;
 }
--- Projeto-em-JS-React/src/components/Account/styles.ts
+++ Projeto-em-JS-React/src/components/Account/styles.ts
@@ -1,2 +1,88 @@
 import styled from 'styled-components';
 
+export const LoginContainer = styled.div`
+    position: fixed;
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
+    background: var(--blue);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+interface LoginContainerProps{
+    isWrong: boolean;
+    isCpfWrong: boolean;
+}
+
+export const LoginForm = styled.form<LoginContainerProps>`
+    display: flex;
+    flex-direction: column;
+    width:100%;
+    max-width: 600px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+
+    h2{
+        color: var(--text-title);
+        font-size: 1.5rem;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+
+    input{
+        width:100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+        border-radius: 0.25rem;
+        border: 1px solid ${(props)=>props.isCpfWrong? 'red' : '#d7d7d7'};
+        background: #e7e9ee;
+
+        font-weight:400;
+        font-size:1rem;
+
+        &::placeholder{
+            color: ${(props)=>props.isCpfWrong? 'red' : 'var(--text-body)'}
+        }
+
+        & + input{
+            margin-top: 1rem;
+        }
+
+        &.senha{
+            border: 1px solid ${(props)=>props.isWrong? 'red' : '#d7d7d7'};
+
+            &::placeholder{
+                color: ${(props)=>props.isWrong? 'red' : 'var(--text-body)'}
+            }
+        }
+
+    }
+
+    button{
+        width: 100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+        background: var(--green);
+        color: #fff;
+        border-radius: 0.25rem;
+        border:0;
+        font-size:1rem;
+        font-weight:600;
+        margin-top: 1.5rem;
+
+        transition: filter 0.2s;
+
+        & + button{
+            margin-top: 0.5rem;
+        }
+
+        &:hover{
+            filter: brightness(0.9);
+        }
+
+    }
+`
